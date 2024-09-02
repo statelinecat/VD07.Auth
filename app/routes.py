e3c53293acca182c15880daed6459a5cbcf8dadb
statelinecat <statelinecat@gmail.com>
2024-09-03 02:53:54 +0300

    add edit

--- app/routes.py
+++ app/routes.py
@@ -47,3 +47,17 @@ def logout():
 @login_required
 def account():
     return render_template('account.html')
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно изменили свои данные!', 'success')
+        return redirect(url_for('login'))
+    return render_template('edit.html', form=form, title='Edit')
