7c9506a565751b5fb128e760bbaec9844569d52b
statelinecat <statelinecat@gmail.com>
2024-09-03 02:14:12 +0300

    add proekt

---
+++ create_db.py
@@ -0,0 +1,7 @@
+from app import db, app
+from app.models import User
+
+
+with app.app_context():
+    db.create_all()
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_wtf import FlaskForm # Это базовый класс для создания форм
-from wtforms import StringField, SubmitField # Это классы для создания полей внутри формы
-from wtforms.validators import DataRequired # Валидатор, нуный для проверки
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
